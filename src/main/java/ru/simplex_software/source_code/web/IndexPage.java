956e1d6b0b194427b76b4456493842f21e2c97fc
Rzhevskiy <dima@rzhevskiy.info>
2017-05-19 16:50:09 +0300

    init commit

---
+++ src/main/java/ru/simplex_software/source_code/web/IndexPage.java
@@ -0,0 +1,16 @@
+package ru.simplex_software.source_code.web;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class IndexPage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public IndexPage(final PageParameters parameters) {
+		super(parameters);
+
+
+
+    }
+}
