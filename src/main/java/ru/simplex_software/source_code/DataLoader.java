ee84a6add36cfb216ffac3a27ad519d48414cd42
Денис <guzikov@yandex.ru>
2017-09-05 18:40:59 +0300

    #886 - добавление записей для предыдущей встречи (dataLoader)

--- src/main/java/ru/simplex_software/source_code/DataLoader.java
+++ src/main/java/ru/simplex_software/source_code/DataLoader.java
@@ -9,8 +9,11 @@ import ru.simplex_software.source_code.model.Meeting;
 import ru.simplex_software.source_code.model.Report;
 import ru.simplex_software.source_code.model.Speaker;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class DataLoader {
 
@@ -24,35 +27,70 @@ public class DataLoader {
     MeetingDAO meetingDAO;
 
     @Transactional
-    public void load() {
+    public void load() throws Exception{
         if (speakerDAO.countAllSpeakers() == 0) {
             Speaker speaker = new Speaker();
             speaker.setSocialId("http://vk.com/dendrito");
-            speaker.setFio("Нахов Пётр Валерьевич");
+            speaker.setFio("Гузиков Денис Сергеевич");
             speakerDAO.saveOrUpdate(speaker);
 
-            List<Report> reportList = new ArrayList<>();
+            List<Report> reportListNew = new ArrayList<>();
 
             Report report = new Report();
             report.setTitle("Современная WEB-разработка");
             report.setAuthor(speaker);
             reportDAO.saveOrUpdate(report);
-            reportList.add(report);
+            reportListNew.add(report);
 
             report = new Report();
             report.setTitle("Алгоритмы. Олимпиадное программирование");
             report.setAuthor(speaker);
             reportDAO.saveOrUpdate(report);
-            reportList.add(report);
+            reportListNew.add(report);
 
             report = new Report();
             report.setTitle("Основы программирования на языке Java");
             report.setAuthor(speaker);
             reportDAO.saveOrUpdate(report);
-            reportList.add(report);
+            reportListNew.add(report);
 
             Meeting meeting = new Meeting();
-            meeting.setReports(reportList);
+            meeting.setReports(reportListNew);
+            meetingDAO.saveOrUpdate(meeting);
+
+//            предыдущие встречи
+            speaker = new Speaker();
+            speaker.setSocialId("http://vk.com/nahov12345");
+            speaker.setFio("Нахов Пётр Валерьевич");
+            speakerDAO.saveOrUpdate(speaker);
+
+            List<Report> reportListPrevious = new ArrayList<>();
+
+            report = new Report();
+            report.setTitle("Spring Core. Понимание основ");
+            report.setAuthor(speaker);
+            reportDAO.saveOrUpdate(report);
+            reportListPrevious.add(report);
+
+            report = new Report();
+            report.setTitle("Популярные шаблоны проектирования");
+            report.setAuthor(speaker);
+            reportDAO.saveOrUpdate(report);
+            reportListPrevious.add(report);
+
+            report = new Report();
+            report.setTitle("Протокол HTTP - базовые понятия");
+            report.setAuthor(speaker);
+            reportDAO.saveOrUpdate(report);
+            reportListPrevious.add(report);
+
+            meeting = new Meeting();
+
+            SimpleDateFormat enterDateFormat = new SimpleDateFormat("MM/d/yy", Locale.ENGLISH);
+            Date date = enterDateFormat.parse("04/20/2017");
+
+            meeting.setDate(date);
+            meeting.setReports(reportListPrevious);
             meetingDAO.saveOrUpdate(meeting);
         }
     }
