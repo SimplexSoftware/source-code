b78cc3df13acdda4ae27c097fe563894d1ace810
DDtKey <medvedev.artem2033@yandex.ru>
2017-09-06 20:45:06 +0300

    #907 Добавление тем выступлений
    Merge branch 'master' of http://git.dev-server.ru/source-code
    
    # Conflicts:
    #	src/main/java/ru/simplex_software/source_code/DataLoader.java

--- src/main/java/ru/simplex_software/source_code/web/IndexPage.java
+++ src/main/java/ru/simplex_software/source_code/web/IndexPage.java
@@ -1,6 +1,8 @@
 package ru.simplex_software.source_code.web;
 
 import net.sf.wicketautodao.model.HibernateQueryDataProvider;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.Model;
@@ -10,6 +12,8 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import ru.simplex_software.source_code.dao.MeetingDAO;
 import ru.simplex_software.source_code.model.Meeting;
+import ru.simplex_software.source_code.model.Report;
+import ru.simplex_software.source_code.security.AuthService;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,7 +22,9 @@ public class IndexPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 	@SpringBean
 	private MeetingDAO meetingDAO;
-
+    @SpringBean
+    private AuthService authService;
+    private Model<String> reportTitleModel=new Model<>("");
 
 	public IndexPage(final PageParameters parameters) {
 		super(parameters);
@@ -38,7 +44,23 @@ public class IndexPage extends WebPage {
 
 		}
 	});
+
+
+        Form<Void> form = new Form<Void>("form"){
+            @Override
+            protected void onSubmit() {
+                Report rep = new Report();
+                rep.setTitle(reportTitleModel.getObject());
+                rep.setAuthor(authService.getLoginnedAccount());
+            }
+        };
+        form.add(new TextField<>("repInput",reportTitleModel));
+
+        add(form);
+
+
     }
+
 
 
 }
