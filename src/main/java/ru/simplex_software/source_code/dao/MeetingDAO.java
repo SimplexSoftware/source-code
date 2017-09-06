5a7e6bb8c0665a551822ea8b8025486604640c6f
DDtKey <medvedev.artem2033@yandex.ru>
2017-09-06 16:41:07 +0300

    #893 Добавлен DataView со старыми встречами
    Merge branch 'master' of http://git.dev-server.ru/source-code
    
    # Conflicts:
    #	src/main/java/ru/simplex_software/source_code/DataLoader.java

--- src/main/java/ru/simplex_software/source_code/dao/MeetingDAO.java
+++ src/main/java/ru/simplex_software/source_code/dao/MeetingDAO.java
@@ -2,8 +2,20 @@ package ru.simplex_software.source_code.dao;
 
 import net.sf.autodao.AutoDAO;
 import net.sf.autodao.Dao;
+import net.sf.autodao.Finder;
+import net.sf.autodao.Named;
 import ru.simplex_software.source_code.model.Meeting;
+
+import java.util.Date;
+import java.util.List;
 
 @AutoDAO
 public interface MeetingDAO extends Dao<Meeting, Long>{
+    @Finder(query = "select count(m) from  Meeting m where date<:date")
+    long getCountPastMetting(@Named("date") Date date);
+
+    @Finder(query = " from  Meeting where date<:date")
+    List<Meeting> findPastMeeting(@Named("date") Date date);
+
+
 }
--- src/main/java/ru/simplex_software/source_code/web/IndexPage.java
+++ src/main/java/ru/simplex_software/source_code/web/IndexPage.java
@@ -1,23 +1,44 @@
 package ru.simplex_software.source_code.web;
 
+import net.sf.wicketautodao.model.HibernateQueryDataProvider;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import ru.simplex_software.source_code.security.AuthService;
+import ru.simplex_software.source_code.dao.MeetingDAO;
+import ru.simplex_software.source_code.model.Meeting;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class IndexPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 	@SpringBean
-	private AuthService authService;
+	private MeetingDAO meetingDAO;
+
 
 	public IndexPage(final PageParameters parameters) {
 		super(parameters);
+		HibernateQueryDataProvider<Meeting, Long> hqDataProvider =
+				new
+				HibernateQueryDataProvider(MeetingDAO.class,"findPastMeeting", Model.of(new Date()));
+
+		add(new DataView<Meeting>("simple", hqDataProvider){
+		@Override
+		protected void populateItem(final Item<Meeting> item)
+		{
+			Meeting meet = item.getModelObject();
+			item.add(new Label("data", String.valueOf(new SimpleDateFormat
+					("dd.MM.yyyy").format(meet.getDate()))));
+			item.add(new Label("report", String.valueOf(meet.getReports().get(0).getTitle())));
+			item.add(new Label("speaker", String.valueOf("Автор: " + meet.getReports().get(0).getAuthor().getFio())));
+
+		}
+	});
     }
 
-	@Override
-	protected void onRender() {
-		super.onRender();
-		authService.getLoginnedAccount();
-	}
+
 }
