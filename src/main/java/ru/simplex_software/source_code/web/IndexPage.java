b95e4364f42d9aa701f8de43248b4416ec240d6e
Денис <guzikov@yandex.ru>
2017-09-08 13:22:34 +0300

    #886 - переход на DataView

--- src/main/java/ru/simplex_software/source_code/web/IndexPage.java
+++ src/main/java/ru/simplex_software/source_code/web/IndexPage.java
@@ -6,9 +6,7 @@ import net.sf.wicketautodao.model.HibernateQueryDataProvider;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.RefreshingView;
 import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -22,10 +20,7 @@ import ru.simplex_software.source_code.model.Report;
 import ru.simplex_software.source_code.security.AuthService;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 
 public class IndexPage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -36,59 +31,38 @@ public class IndexPage extends WebPage {
 	private ReportDAO reportDAO;
     @SpringBean
     private AuthService authService;
+
     private Model<String> reportTitleModel=new Model<>("");
 
 	public IndexPage(final PageParameters parameters) throws ParseException {
 		super(parameters);
 
-		Meeting meeting0 = meetingDAO.findNewMeeting(new Date()).get(0);
-		SimpleDateFormat enterDateFormat = new SimpleDateFormat("d.MM.yyyy");
-		String date = enterDateFormat.format(meeting0.getDate());
-
-		/*HibernateModel<Meeting> meeting = new HibernateModel<>(meetingDAO.findNewMeeting(new Date()));
-		HibernateModelList<Report> reports = new HibernateModelList<>(meeting.getObject().getReports());
+		HibernateModel<Meeting> meeting = new HibernateModel<>(meetingDAO.findNewMeeting(new Date()).get(0));
 
 		SimpleDateFormat enterDateFormat = new SimpleDateFormat("d.MM.yyyy");
 		String date = enterDateFormat.format(meeting.getObject().getDate());
-*/
+
+		HibernateModelList<Report> reports = new HibernateModelList<>(meeting.getObject().getReports());
+
 		add(new Label("meetingDate", date));
 
-		/*add(new ListView<Report>("newMeeting", reports) {
+		HibernateQueryDataProvider<Meeting, Long> hqDataProviderNewMeeting =
+				new	HibernateQueryDataProvider(MeetingDAO.class,"findNewMeeting", Model.of(new Date()));
+
+		DataView<Meeting> dataView = new DataView<Meeting>("newMeeting", hqDataProviderNewMeeting) {
 			@Override
-			public void populateItem(ListItem<Report> item) {
+			protected void populateItem(Item<Meeting> item) {
 
+				Report report = item.getModel().getObject().getReports().iterator().next();
 
-				Report report = item.getModel().getObject();
-
-				item.add(new MultiLineLabel("report", String.valueOf(report.getTitle())));
-				item.add(new Label("speaker", String.valueOf("Автор: " + report.getAuthor().getFio())));
-				item.add(new Label("more", "подробнее"));
-			}
-		});*/
-
-		add(new RefreshingView<Report>("newMeeting") {
-			@Override
-			protected void populateItem(Item<Report> item) {
-
-				Report report = item.getModel().getObject();
-				item.add(new MultiLineLabel("report", String.valueOf(report.getTitle())));
+				item.add(new MultiLineLabel("report", report.getTitle()));
 				item.add(new Label("speaker",  report.getAuthor().getFio()));
+
 			}
+		};
 
-			@Override
-			protected Iterator<IModel<Report>> getItemModels() {
-				Meeting meeting = meetingDAO.findNewMeeting(new Date()).get(0);
+		add(dataView);
 
-				List<Report> reports = meeting.getReports();
-				final List<IModel<Report>> reportModel = new ArrayList<IModel<Report>>();
-
-				for(Report report: reports){
-					reportModel.add(Model.of(report));
-				}
-
-				return reportModel.iterator();
-			}
-		});
 
 		HibernateQueryDataProvider<Meeting, Long> hqDataProviderPrevMeeting =
 				new	HibernateQueryDataProvider(MeetingDAO.class,"findPastMeeting", Model.of(new Date()));
@@ -103,10 +77,8 @@ public class IndexPage extends WebPage {
 						("dd.MM.yyyy").format(meet.getDate()))));
 				item.add(new Label("report", String.valueOf(meet.getReports().get(0).getTitle())));
 				item.add(new Label("speaker", String.valueOf("Автор: " + meet.getReports().get(0).getAuthor().getFio())));
-
 		    }
 	    });
-
 
         Form<Void> form = new Form<Void>("form"){
             @Override
@@ -120,11 +92,9 @@ public class IndexPage extends WebPage {
 				meetingDAO.saveOrUpdate(meeting.getObject());
 
                 reportTitleModel.setObject("");
-
             }
         };
         form.add(new TextField<>("repInput",reportTitleModel));
-
 
         add(form);
 
