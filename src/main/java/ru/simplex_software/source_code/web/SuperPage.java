a74beb5fd670481ca7fbcdb4ba2c80baaa54511a
Anton Shelenkov <a.shelenkov@simplex-software.ru>
2017-10-27 11:10:30 +0300

    #1090 Added login/logout function

--- src/main/java/ru/simplex_software/source_code/web/SuperPage.java
+++ src/main/java/ru/simplex_software/source_code/web/SuperPage.java
@@ -1,9 +1,21 @@
 package ru.simplex_software.source_code.web;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import ru.simplex_software.source_code.security.AuthService;
 
 public abstract class SuperPage extends WebPage {
+
+    @SpringBean
+    private AuthService authService;
+
     public SuperPage() {
         add(new MenuPanel("menuPanel"));
+        if (authService.getLoginnedAccount() == null) {
+            add(new Fragment("logArea", "login", this));
+        } else {
+            add(new Fragment("logArea", "logout", this));
+        }
     }
 }
