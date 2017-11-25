8db275add35fa22ec45290aff7fde1e1c39f490d
Max <m.vrady@simplex-software.ru>
2017-11-25 14:51:22 +0300

    #1241 Сделано добавление интересных тем на главной странице(только для авторизованных пользователей)

--- src/main/java/ru/simplex_software/source_code/dao/TagDAO.java
+++ src/main/java/ru/simplex_software/source_code/dao/TagDAO.java
@@ -3,6 +3,7 @@ package ru.simplex_software.source_code.dao;
 import net.sf.autodao.AutoDAO;
 import net.sf.autodao.Dao;
 import net.sf.autodao.Finder;
+import net.sf.autodao.Named;
 import ru.simplex_software.source_code.model.Tag;
 
 import java.util.List;
@@ -11,4 +12,7 @@ import java.util.List;
 public interface TagDAO extends Dao<Tag, Long> {
     @Finder(query = "FROM Tag ORDER BY name")
     List<Tag> findAllTags();
+
+    @Finder(query = "FROM Tag WHERE name = :name")
+    Tag getTag(@Named("name") String name);
 }
--- src/main/java/ru/simplex_software/source_code/web/WishesPanel.java
+++ src/main/java/ru/simplex_software/source_code/web/WishesPanel.java
@@ -2,17 +2,31 @@ package ru.simplex_software.source_code.web;
 
 
 import net.sf.wicketautodao.model.HibernateQueryDataProvider;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.springframework.util.StringUtils;
 import ru.simplex_software.source_code.dao.TagDAO;
 import ru.simplex_software.source_code.model.Tag;
+import ru.simplex_software.source_code.security.AuthService;
 
 /**
  * Panel that displays all interesting tags for user.
  */
 public class WishesPanel extends Panel {
+
+    @SpringBean
+    private AuthService authService;
+
+    @SpringBean
+    private TagDAO tagDAO;
 
     public WishesPanel(String id, IModel<?> model) {
         super(id, model);
@@ -27,5 +41,39 @@ public class WishesPanel extends Panel {
                     "interestingTagPanel", item.getModel()));
             }
         });
+
+        Fragment formFragment;
+        if (authService.getLoginnedAccount() != null) {
+            formFragment = new Fragment("fragmentContent", "form", this);
+
+            formFragment.add(new FeedbackPanel("message"));
+
+            TextField textField = new TextField("wishes-input", Model.of(""));
+
+            Form wishesForm = new Form<Void>("wishes-form") {
+                @Override
+                protected void onSubmit() {
+                    String tagName = (String) textField.getModel().getObject();
+                    if (!StringUtils.hasLength(tagName)) {
+                        info("Имя темы не может быть пустым");
+                        return;
+                    }
+                    if (tagDAO.getTag(tagName) == null) {
+                        Tag tag = new Tag();
+                        tag.setName(tagName);
+                        tagDAO.saveOrUpdate(tag);
+                    }else {
+                        info("Тема " + tagName + " уже содержится");
+                    }
+                }
+            };
+            wishesForm.add(textField);
+            formFragment.add(wishesForm);
+
+        }else {
+            formFragment = new Fragment("fragmentContent", "empty", this);
+        }
+        add(formFragment);
+
     }
 }
